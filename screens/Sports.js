8f15cf68155039be6c09430ad4cee39b119728e0
fabihaMU47 <fabihasyeda084@gmail.com>
2021-10-15 18:45:55 +0600

    5 screens with icon and header

---
+++ screens/Sports.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function SportsScreen(){
+    return (
+        <View>
+        <View style={styles.container}>
+            <Text style={styles.text}>Sports</Text>
+        </View>
+        </View>
+    )
+}
+
+const  styles = StyleSheet.create ({
+    container: {
+        textAlign: 'center',
+        padding: 10,
+        backgroundColor: '#e0e0e0',
+        border: '1px solid #bdbdbd'
+    },
+    text: {
+        fontSize: 24,
+        fontStyle: 900,
+    }
+})
